fbaad9c0fa5152b2cc4d4ba6ccd5c50aaea7ef22
Simon Smallchua <40650011+simonsmallchua@users.noreply.github.com>
2025-10-07 20:50:24 +1100

    Change icon to SVG

--- web/static/js/bb-metadata.js
+++ web/static/js/bb-metadata.js
@@ -117,7 +117,11 @@ class MetricsMetadata {
       // Create info icon
       const infoIcon = document.createElement("span");
       infoIcon.className = "bb-info-icon";
-      infoIcon.textContent = "ℹ";
+      infoIcon.innerHTML = `
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" aria-hidden="true" focusable="false">
+          <path d="M16 3C8.832 3 3 8.832 3 16s5.832 13 13 13 13-5.832 13-13S23.168 3 16 3zm0 2c6.086 0 11 4.914 11 11s-4.914 11-11 11S5 22.086 5 16 9.914 5 16 5zm-1 5v2h2v-2zm0 4v8h2v-8z" />
+        </svg>
+      `.trim();
       infoIcon.setAttribute("data-bbb-tooltip", info);
       infoIcon.setAttribute("aria-label", "More information");
 
